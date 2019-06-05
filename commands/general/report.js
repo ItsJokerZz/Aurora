e20fb0e53b34cf97c4559cb1bb6d7e757dddb165
ItsJokerZz <ItsJokerZz@Gmail.com>
2019-06-05 01:41:25 -0400

    fixed issues

--- commands/general/report.js
+++ commands/general/report.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 let recent = new Set();
 
 module.exports.run = async (client, message, args) => {
-    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])) || message.author;
     let channel = message.guild.channels.find(channel => channel.name === "bot-logs");
     let reason = args.join(" ").slice(22);
 
@@ -42,38 +42,38 @@ module.exports.run = async (client, message, args) => {
                         title: "You must wait 5 minutes before using this command again!"
                     }
                 })
-            }
-            if (!args[1]) {
-                let embed = new Discord.RichEmbed()
-                    .setColor("#0x9400d3")
-                    .setDescription("You must include a reason.")
-                message.channel.send(embed);
             } else {
-                let embed = new Discord.RichEmbed()
-                    .setColor("#0x9400d3")
-                    .addField("Reported User", `${user}`)
-                    .addField("Reported By", `<@${message.author.id}>`)
-                    .addField("Reported In", message.channel)
-                    .addField("Reason", reason);
-
-                if (channel) {
-                    channel.send(embed);
+                if (!args[1]) {
+                    let embed = new Discord.RichEmbed()
+                        .setColor("#0x9400d3")
+                        .setDescription("You must include a reason.")
+                    message.channel.send(embed);
                 } else {
-                    if (!channel) {
-                        embed.setDescription("Log messages will be sent in the channel the command was ran.\n" +
-                            "If you wish to change that create a channel called bot-logs.");
-                        message.channel.send(embed);
+                    let embed = new Discord.RichEmbed()
+                        .setColor("#0x9400d3")
+                        .addField("Reported User", `${user}`)
+                        .addField("Reported By", `<@${message.author.id}>`)
+                        .addField("Reported In", message.channel)
+                        .addField("Reason", reason);
+
+                    if (channel) {
+                        channel.send(embed);
+                    } else {
+                        if (!channel) {
+                            embed.setDescription("Log messages will be sent in the channel the command was ran.\n" +
+                                "If you wish to change that create a channel called bot-logs.");
+                            message.channel.send(embed);
+                        }
                     }
+                    recent.add(user.id);
                 }
-                recent.add(user.id);
             }
         }
+        setTimeout(() => {
+            recent.delete(user.id);
+        }, 300000);
     }
 }
-
-setTimeout(() => {
-    recent.delete(user.id);
-}, 300000);
 
 module.exports.help = {
     name: "report"

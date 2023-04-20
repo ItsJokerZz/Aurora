7cefa3c5c9552f835dd75a074582bd0a40f49495
ItsJokerZz <ItsJokerZz@Gmail.com>
2023-04-20 04:33:42 -0400

    added missing commands

---
+++ source/commands/bot/ping.js
@@ -0,0 +1,33 @@
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    aliases: ["latency", "delay", "responce"],
+
+    run: async (client, message) => {
+        const msg = await message.reply("Pinging...")
+
+        const embed = new EmbedBuilder()
+            .setColor(0x0AA0A0)
+            .setTitle("Latency Information")
+            .addFields({
+                name: "🤖 Client (Message)",
+                value: `** ${new Date().getTime() - msg.createdTimestamp} ms ⌛**`,
+                inline: true
+            }, {
+                name: "🖥️ API (Discord)",
+                value: `**${client.ws.ping} ms ⌛**`,
+                inline: true
+            }).setFooter({
+                text: "Results MAY vary depending on area, usage, time, etc."
+            })
+
+        msg.delete().then(() => {
+            message.reply({
+                embeds: [embed]
+            })
+        })
+    }
+}
